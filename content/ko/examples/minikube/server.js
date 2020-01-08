6fbc708b9dea2f2f11f38e03cf8e0c678fdfbe18
Supriya Sirbi <supriya.sirbi@gmail.com>
2020-01-08 15:25:57 +0530

    Issue #18498 translate URL Request to Korean

--- content/ko/examples/minikube/server.js
+++ content/ko/examples/minikube/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('URL 요청을 받았습니다 ' + request.url);
   response.writeHead(200);
   response.end('Hello World!');
 };
